6ba840499d41980a036c20ca7840590f9818afaa
Arnold Pfahnl <ajpfahnl@gmail.com>
2021-12-17 14:56:45 -0800

    restore files before merge

--- snowjam21/Assets/scripts/enemy.cs
+++ snowjam21/Assets/scripts/enemy.cs
@@ -5,33 +5,99 @@ using UnityEngine;
 public class enemy : MonoBehaviour
 {
     public int hp;
+    public int burnTime;
+    public ParticleSystem onFire;
+    public selfDest onFireSelfDest;
     public Transform trfm;
+    public Rigidbody2D rb;
     public GameObject death;
-    
+
+    public SpriteRenderer flashRend;
+    public Color flashCol;
+
+    public bool noKB;
+    public bool untoasted;
+    public SpriteRenderer rend;
+    Color darken;
+
     void Start()
     {
         trfm = transform;
         trfm.parent = null;
+
+        if (untoasted) { darken = new Color(.2f, .2f, .2f, 0); }
     }
 
-    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (burnTime > 0)
+        {
+            burnTime--;
+            if (burnTime == 0) { onFire.Stop(); }
+            if (burnTime%20==0)
+            {
+                takeDmg();
+            }
+            if (trfm.position.y < mainCam.trfm.position.y - 4.5f)
+            {
+                score.addPoint(); Destroy(gameObject);
+                Instantiate(death, trfm.position, trfm.rotation);
+            }
+        } else
+        {
+            if (trfm.position.y < mainCam.trfm.position.y - 4.5f)
+            {
+                Destroy(gameObject);
+                Instantiate(death, trfm.position, trfm.rotation);
+            }
+        }
+        if (flashCol.a > 0)
+        {
+            flashCol.a -= .05f;
+            flashRend.color = flashCol;
+        }
+    }
 
+    void takeDmg()
+    {
+        if (untoasted)
+        {
+            rend.color -= darken;
+        }
+        hp -= 20;
+        if (hp <= 0)
+        {
+            Instantiate(death, trfm.position, trfm.rotation);
+            
+            onFire.transform.parent = null;
+            onFire.Stop();
+            onFireSelfDest.enabled = true;
+            score.addPoint();
+            Destroy(gameObject);
+        } else
+        {
+            flashCol.a = .8f;
+            flashRend.color = flashCol;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.layer == 9)
+        if (col.gameObject.layer == 11)
         {
-            Instantiate(death, trfm.position, trfm.rotation);
-            Destroy(gameObject);
+            if (burnTime < 1) { onFire.Play(); if (!noKB) { knockback(playerMovement.trfm.position, 8); } }
+            takeDmg();
+            burnTime = 100;
         }
         
     }
-    private void OnCollisionEnter2D(Collision2D col)
+
+    Quaternion original;
+    void knockback(Vector2 source, int force)
     {
-        if (col.gameObject.layer == 7)
-        {
-            Destroy(col.gameObject);
-            gameState.doLose();
-        }
+        original = trfm.rotation;
+        trfm.rotation = Quaternion.AngleAxis(Mathf.Atan2(trfm.position.y - source.y, trfm.position.x - source.x) * Mathf.Rad2Deg, Vector3.forward);
+        rb.velocity = trfm.right * force;
+        trfm.rotation = original;
     }
 }
--- snowjam21/Assets/scripts/mainCam.cs
+++ snowjam21/Assets/scripts/mainCam.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class mainCam : MonoBehaviour
 {
     public Transform plyrTrfm;
+    public static mainCam camScr;
     public static Transform trfm;
     Vector3 pos;
+    float trauma;
+    public static float riseRate;
 
+    private void Awake()
+    {
+        camScr = GetComponent<mainCam>();
+        riseRate = .03f;
+    }
     private void Start()
     {
         trfm = transform;
@@ -17,17 +25,54 @@ public class mainCam : MonoBehaviour
     {
         if (gameState.started && !gameState.lost)
         {
+            traumaSystem();
             pos.x = (plyrTrfm.position.x - trfm.position.x) * .1f;
-            pos.y += .03f;
+            pos.y += riseRate;
             trfm.position = pos;
-
-            if(trfm.position.y - plyrTrfm.position.y > 8)
-            {
-                gameState.doLose();
-            }
+            
         } else if (Input.anyKey)
         {
             gameState.doStart();
         }
+    }
+
+    public static void addTrauma(float pTrauma)
+    {
+        if (camScr.trauma < pTrauma)
+        {
+            float tempTrauma = pTrauma;
+            pTrauma -= (pTrauma - camScr.trauma);
+            camScr.trauma = tempTrauma;
+            camScr.trauma += pTrauma / 5;
+        }
+        else
+        {
+            camScr.trauma += pTrauma / 5;
+        }
+    }
+
+    float zAngle;
+    Vector3 recalibrate;
+    void traumaSystem()
+    {
+        if (trauma > 0)
+        {
+            trauma -= 1;
+            trfm.position += new Vector3(Random.Range(-trauma / 120, trauma / 120), Random.Range(-trauma / 80, trauma / 80), 0);
+            trfm.Rotate(Vector3.forward * Random.Range(-trauma / 20, trauma / 20));
+        }
+        zAngle = trfm.localEulerAngles.z;
+        if (zAngle > .3f && zAngle < 180)
+        {
+            trfm.Rotate(Vector3.forward * -.5f);
+        }
+        if (zAngle > 180 && zAngle < 359.5f)
+        {
+            trfm.Rotate(Vector3.forward * .5f);
+        }
+
+        recalibrate.x = -trfm.position.x / 10;
+        recalibrate.y = -trfm.position.y / 10;
+        trfm.position += recalibrate;
     }
 }
