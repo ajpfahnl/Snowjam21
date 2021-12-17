258a80665eea4378cfde341163aa5e6f969e98e6
Faustine Wang <faustinex.wang@gmail.com>
2021-12-17 03:41:30 -0800

    UI buttons coding begins

---
+++ snowjam21/Assets/scripts/Faustine/SoundManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] AudioSource[] sounds;
+
+    public void SetBGM(bool toggle){
+        sounds[0].volume = 0;
+    }
+
+    public void SetSFX(bool toggle){
+    }
+
+}
---
+++ snowjam21/Assets/scripts/Faustine/UIScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIScript : MonoBehaviour
+{
+    [Header("UI Elements")]
+    [SerializeField] GameObject howToPlay;
+    [SerializeField] Button bgm, sfx;
+    [SerializeField] Sprite[] images; //set in inspector
+    //[SerializeField] SoundManager soundManager;
+    private bool bgmOn = true;
+    private bool sfxOn = true;
+
+    private void Update()
+    {
+        if (bgmOn)
+        {
+            bgm.image.sprite = images[0]; //set bgm icon to be on
+        }
+        else
+        {
+            bgm.image.sprite = images[1]; //set bgm icon to be off
+        }
+
+        if (sfxOn)
+        {
+            sfx.image.sprite = images[2]; //set sfx icon to be on
+        }
+        else
+        {
+            sfx.image.sprite = images[3]; //set sfx icon to be off
+        }
+
+        /*
+        soundManager.SetBGM(bgmOn);
+        soundManager.SetSFX(sfxOn);
+         */
+    }
+
+    public void Back()
+    {
+        howToPlay.SetActive(false);
+    }
+
+    public void HowToPlay()
+    {
+        howToPlay.SetActive(true);
+    }
+
+    public void ToggleBGM() //toggle when clicked on
+    {
+        bgmOn = !bgmOn;
+        Debug.Log(bgmOn);
+    }
+
+    public void ToggleSFX()
+    {
+        sfxOn = !sfxOn;
+        Debug.Log(sfxOn);
+    }
+}
